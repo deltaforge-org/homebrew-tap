46bc07418327318aae17e0d391b253c8088ff7ca
DeltaForge Releases <releases@deltaforge.org>
2026-06-12 16:17:18 +0200

    Bump DeltaForge products to 1.0.5 (products: odbc)

--- Casks/deltaforge-odbc.rb
+++ Casks/deltaforge-odbc.rb
@@ -1,6 +1,6 @@
 cask "deltaforge-odbc" do
   version "1.0.5"
-  sha256 "c583d5a9f8f6c328b14cdcea26cface2bd2acc1f00bd706ecf6aa7933b51b8e6"
+  sha256 "8a83aa300aa48fc23308cf9781ad4aaef227faa6e71485f5b021662e74eba1f5"
   url "https://github.com/deltaforge-org/delta-forge-odbc/releases/download/v1.0.5/deltaforge-odbc-1.0.5-macos-universal.pkg"
   name "DeltaForge ODBC Driver"
   desc "ODBC 3.80 driver for DeltaForge Delta Lake and Iceberg tables"
