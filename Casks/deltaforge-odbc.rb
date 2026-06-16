28c82484eb08293aa9ce3f0a6800c44954a8bbfa
DeltaForge Releases <releases@deltaforge.org>
2026-06-16 10:47:28 +0200

    Bump DeltaForge products to 1.0.610 (products: odbc)

--- Casks/deltaforge-odbc.rb
+++ Casks/deltaforge-odbc.rb
@@ -1,10 +1,10 @@
 cask "deltaforge-odbc" do
-  version "1.0.6"
-  sha256 "fb82f8c7c8905f5ea1d2f96c4578d9bf737f8ebcff60db02b942b6959c9ec50e"
-  url "https://github.com/deltaforge-org/delta-forge-odbc/releases/download/v1.0.6/deltaforge-odbc-1.0.6-macos-universal.pkg"
+  version "1.0.610"
+  sha256 "de24e172020c3df4ec4bb90d13f3596445cee5c860b5aea26dae4e744bf6cc15"
+  url "https://github.com/deltaforge-org/delta-forge-odbc/releases/download/v1.0.610/deltaforge-odbc-1.0.610-macos-universal.pkg"
   name "DeltaForge ODBC Driver"
   desc "ODBC 3.80 driver for DeltaForge Delta Lake and Iceberg tables"
   homepage "https://deltaforge.org/odbc"
-  pkg "deltaforge-odbc-1.0.6-macos-universal.pkg"
+  pkg "deltaforge-odbc-1.0.610-macos-universal.pkg"
   uninstall pkgutil: "org.deltaforge.odbc"
 end
